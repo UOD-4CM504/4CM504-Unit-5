c2a9c589cebc6a34adbb86bca47ff0c2453e1b4a
Sam O'Neill <38918777+samtoneill@users.noreply.github.com>
2023-11-16 21:06:22 +0000

    Update exercise_5_1_test_2.py

--- 5-10-Exercise-1/exercise_5_1_test_2.py
+++ 5-10-Exercise-1/exercise_5_1_test_2.py
@@ -1,3 +1,5 @@
-def test_calc_retail_price():
+from exercise_5_1 import *
+
+def test_calc_retail_price():-
   assert calc_retail_price(5,40) == 7.0
   assert round(calc_retail_price(25.3,75),3) == 44.275
