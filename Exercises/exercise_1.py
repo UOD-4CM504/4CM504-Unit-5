31450e4711b21e2a909b406c225145beaa34c15b
jungian6 <122241019+jungian6@users.noreply.github.com>
2024-07-22 15:56:35 +0100

    Update exercise_1.py

--- Exercises/exercise_1.py
+++ Exercises/exercise_1.py
@@ -1,14 +1,18 @@
 def calc_retail_price(wholesale, markup):
-  return wholesale * (1 + markup/100)
+    # remove the following line and write your function here
+    pass
+
 
 def main():
-  wholesale = input("Please enter a wholesale cost:\n")
-  markup = input("Please enter a markup percentage:\n")
-  retail = calc_retail_price(float(wholesale), float(markup))
-  print(f"The retail price is £{retail:.2f}.")
-  
+    # remove the following line and write your main program here
+    pass
 
-# This is needed for the tests. This is the now the first bit of code Python will run.
+
+# This is needed for the tests. This is now the first bit of code Python will run.
 # Please ask if you are curious, but it is enough to understand that this calls the main() function and runs whatever code you have in there.
 if __name__ == "__main__":
-  main() # calls the main function
+    main()  # calls the main function
+
+    # uncomment the following lines to test calc_retail_price when you run the program.You will need to comment out main()
+    # print(calc_retail_price(3.00, 100)) # should print 6.0
+    # print(calc_retail_price(5.00, 50)) # should print 7.5
